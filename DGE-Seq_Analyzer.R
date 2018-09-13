ad6ef94bb336646b9ddb936e17cdc422cad9266e
E114424Z <dimitri.meistermann@etu.univ-nantes.fr>
2018-09-13 18:53:57 +0200

    fix local fdr statistic

--- DGE-Seq_Analyzer.R
+++ DGE-Seq_Analyzer.R
@@ -245,13 +245,13 @@ FDR.res<-list()
 for(comp in comps){
   DE[[comp]]<-data.frame(res[[comp]])
   rownames(DE[[comp]])<-genes
-  FDR.res[[comp]]<-fdrtool(res[[comp]]$pvalue[which(!is.na(res[[comp]]$pvalue))], statistic= "normal", plot = F)
+  FDR.res[[comp]]<-fdrtool(res[[comp]]$pvalue[which(!is.na(res[[comp]]$pvalue))], statistic= "pvalue", plot = F)
   for(item in c("pval","qval","lfdr")){
     stat<-FDR.res[[comp]][[item]]
     FDR.res[[comp]][[item]]<-rep(NA,length(genes))
     FDR.res[[comp]][[item]][which(!is.na(res[[comp]]$pvalue))]<-stat
   }
-  DE[[comp]]$localFDR<-FDR.res[[comp]]$qval
+  DE[[comp]]$localFDR<-FDR.res[[comp]]$lfdr
 }
 
 #sample/group correspondence 
